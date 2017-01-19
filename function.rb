3a0e0ef267f1338a680e5dea98b9be91f43cf043
Chad Swatsworth <Chad Swatsworth>
2017-01-19 15:39:59 -0500

    testfunction_practice

---
+++ function.rb
@@ -0,0 +1,7 @@
+def function_one(number)
+	if number == 3
+		'mined'
+	else
+		number
+	end
+end
--- testfunction.rb
+++ testfunction.rb
@@ -4,4 +4,10 @@ class TestMMFunctions < Minitest::Test
 	def test_one_equals_one
 		assert_equal(1,1)
 	end
+	def test_one_returns_one
+		assert_equal(1, function_one(1))
+	end
+	def test_three_returns_mined
+		assert_equal('mined',function_one(3))
+	end
 end
